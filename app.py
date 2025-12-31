a3c3a44cfa7dfa01829e8a87ddb0069b86e5a2b4
KaiTutor <miltonkaitutor@gmail.com>
2025-12-30 21:22:30 -0500

    added csv to db

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 # import models
 from models import Base, session, Book, engine
+import datetime
+import csv
 
-# main menu - add, search, analysis, exit, view
+def menu():
+	while True:
+		print('''
+			\nPROGRAMMING BOOKS
+			\r1) ADD BOOK
+			\r2) VIEW ALL BOOKS
+			\r3) SEARCH FOR BOOKS
+			\r4) BOOK ANALYSIS
+			\r5) EXIT
+			\n''')
+		choice = input('What would you like to do? ')
+		if choice in ['1', '2', '3', '4', '5']:
+			return choice
+		else:
+			input('''
+				\rPlease choose one of the options above
+				\rA number 1-5.
+				\rPress enter to try again 
+				\n''')
+
 
 # add books to database
 
@@ -15,5 +36,80 @@ from models import Base, session, Book, engine
 
 # loop runs our program 
 
+
+def clean_date(date_str):
+	months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+	
+	split_date = date_str.split(' ')
+	month = int(months.index(split_date[0]) + 1)
+	day = int(split_date[1].split(',')[0])
+	year = int(split_date[2])
+
+	return datetime.date(year, month, day)
+
+
+def clean_price(price_str):
+	price_float = float(price_str)
+	return int(price_float * 100)
+
+
+def add_csv():
+	with open("suggested_books.csv") as csvfile:
+		data = csv.reader(csvfile)
+
+		for row in data:
+			# checks if book is there returns none if not
+			book_in_db = session.query(Book).filter(Book.title==row[0]).one_or_none()
+
+			if book_in_db == None:
+				title = row[0]
+				author = row[1]
+				date = clean_date(row[2])
+				price = clean_price(row[3])
+				new_book = Book(title=title, author=author, published_date=date, price=price)
+				session.add(new_book)
+
+		session.commit()
+
+
+def app():
+	app_running = True
+
+	while app_running:
+		choice = menu()
+
+		if choice == '1':
+			# add book
+			pass
+		elif choice == '2':
+			# view books
+			pass
+		elif choice == '3':
+			# search for books
+			pass
+		elif choice == '4':
+			# book analysis
+			pass
+		elif choice == '5':
+			# exit
+			print("Goodbye")
+			app_running = False
+		else:
+			print("something wrong with choice")
+			app_running = False
+
+
 if __name__ == '__main__':
 	Base.metadata.create_all(engine)
+	#app()
+	add_csv()
+
+	for book in session.query(Book):
+		print(book)
+
+
+
+
+
+
+
