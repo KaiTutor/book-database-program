bab281a303d765f442fc5979ef589e5bab0309fa
KaiTutor <miltonkaitutor@gmail.com>
2025-12-30 19:34:02 -0500

    Init

---
+++ app.py
@@ -0,0 +1,15 @@
+# import models
+
+# main menu - add, search, analysis, exit, view
+
+# add books to database
+
+# edit books
+
+# delete books
+
+# search 
+
+# data cleaning 
+
+# loop runs our program 
